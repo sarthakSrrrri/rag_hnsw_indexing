6693c307f86979fd1eff85a3f945235be1957bf2
sarthaksrrri <sarthaksrrrivastava@gmail.com>
2026-01-15 12:14:12 +0530

    change in model

--- embed.py
+++ embed.py
@@ -1,21 +1,28 @@
+print("Before import")
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+os.makedirs("data" , exist_ok=True)
 from sentence_transformers import SentenceTransformer
+print("After import")
+
 import numpy as np
 
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+print("Model loaded")
 
+with open("docs/doc1.txt", "r") as f:
 
-
-model = SentenceTransformer("sentence-transformer/all-MiniLM-L6-v2")
-
-
-with open("doc.txt" , mode = 'r') as f:
     documents = [line.strip() for line in f if line.strip()]
 
 embedding = model.encode(
     documents,
-    convert_to_numpy = True,
-    show_progress_bar = True
+    convert_to_numpy=True,
+    show_progress_bar=True
 )
 
-np.save("data/emebeddin.npy" , embedding)
 
-print(f"Embedding Shape {embedding.shape}")
+with open("data", "w") as f:
+    np.save("data/embeddings.npy", embedding)
+print("Embedding shape:", embedding.shape)
